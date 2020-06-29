c1a52783270b834711806a223af4463199464539
Joao Victor <joaovictor.juliano@gmail.com>
2020-06-29 15:54:41 -0300

    OrderStatus enum

--- src/main/java/com/coursespring/course/config/TestConfig.java
+++ src/main/java/com/coursespring/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.coursespring.course.entities.Order;
 import com.coursespring.course.entities.User;
+import com.coursespring.course.entities.enums.OrderStatus;
 import com.coursespring.course.repositories.OrderRepository;
 import com.coursespring.course.repositories.UserRepository;
 
@@ -19,7 +20,8 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-
+	
+	@Autowired
 	private OrderRepository orderRepository;
 
 	@Override
@@ -27,13 +29,13 @@ public class TestConfig implements CommandLineRunner {
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-		
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
+
 	}
 }
